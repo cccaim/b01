bbb194c8fbeb8f58dc261d450b8d925122bac0dd
0Hoxy <tinyfinger@naver.com>
2024-07-08 15:24:13 +0900

    crud 삭제 테스트

--- src/main/java/org/zerock/b01/service/BoardServiceImpl.java
+++ src/main/java/org/zerock/b01/service/BoardServiceImpl.java
@@ -44,4 +44,9 @@ public class BoardServiceImpl implements BoardService {
     board.change(boardDTO.getTitle(), boardDTO.getContent());
     boardRepository.save(board);
   }
+
+  @Override
+  public void remove(Long bno){
+    boardRepository.deleteById(bno);
+  }
 }
