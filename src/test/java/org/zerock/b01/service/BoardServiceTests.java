7be29dba66d664a6f17d47fcdbefc52332053214
0Hoxy <tinyfinger@naver.com>
2024-07-09 10:07:53 +0900

    테스트 코드 검색기능 확인

--- src/test/java/org/zerock/b01/service/BoardServiceTests.java
+++ src/test/java/org/zerock/b01/service/BoardServiceTests.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.b01.domain.Board;
 import org.zerock.b01.dto.BoardDTO;
+import org.zerock.b01.dto.PageRequestDTO;
+import org.zerock.b01.dto.PageResponseDTO;
 
 @SpringBootTest
 @Log4j2
@@ -38,5 +40,18 @@ public class BoardServiceTests {
             .content("Updated content 101...")
             .build();
     boardService.modify(boardDTO);
+  }
+
+  @Test
+  public void testList(){
+    PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+            .type("tcw")
+            .keyword("1")
+            .page(1)
+            .size(10)
+            .build();
+    PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
+
+    log.info("responseDTO: " + responseDTO);
   }
 }
