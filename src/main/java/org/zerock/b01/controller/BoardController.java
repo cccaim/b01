47e673ee311e7a19addcedef671237561dac8899
0Hoxy <tinyfinger@naver.com>
2024-07-10 09:22:09 +0900

    modify 포스트매핑

--- src/main/java/org/zerock/b01/controller/BoardController.java
+++ src/main/java/org/zerock/b01/controller/BoardController.java
@@ -59,4 +59,21 @@ public class BoardController {
     log.info(boardDTO);
     model.addAttribute("dto", boardDTO);
   }
+
+  @PostMapping("/modify")
+  public String modify(PageRequestDTO pageRequestDTO, @Valid BoardDTO boardDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+    log.info("board POST modify..." + boardDTO);
+
+    if(bindingResult.hasErrors()) {
+      log.info("has errors....");
+      String link = pageRequestDTO.getLink();
+      redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+      redirectAttributes.addAttribute("bno",boardDTO.getBno());
+      return "redirect:/board/modify?+link";
+    }
+    boardService.modify(boardDTO);
+    redirectAttributes.addFlashAttribute("result","modified");
+    redirectAttributes.addAttribute("bno",boardDTO.getBno());
+    return  "redirect:/board/read";
+  }
 }
